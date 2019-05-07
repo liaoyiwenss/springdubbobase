27a4c10b29aca23d58b88de9768681f017913599
liaoyiwen <953013493@qq.com>
2019-05-07 22:13:12 +0800

    sadad

--- src/main/java/net/wanho/PringbootdemoApplication.java
+++ src/main/java/net/wanho/PringbootdemoApplication.java
@@ -1,11 +1,17 @@
 package net.wanho;
 
+import com.github.tobato.fastdfs.FdfsClientConfig;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableMBeanExport;
+import org.springframework.context.annotation.Import;
+import org.springframework.jmx.support.RegistrationPolicy;
 
 @SpringBootApplication
+@Import(FdfsClientConfig.class)
+@EnableMBeanExport(registration = RegistrationPolicy.IGNORE_EXISTING)
 @MapperScan("net.wanho.mapper")
 public class PringbootdemoApplication {
 
